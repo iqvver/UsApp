1f3881b82dc720463377ac73d0425dcf922c8c86
iqvverVIVO <iqvver@gmail.com>
2022-09-22 11:33:09 +0200

    modal first

--- src/Componets/Modal/ModalFilter.jsx
+++ src/Componets/Modal/ModalFilter.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import close from "../../Assets/icons/close.svg";
 
 const ModalFilter = () => {
   return (
-    <div>ModalFilter</div>
-  )
-}
+    <div className="modal">
+      <div className="modal__title">Сортировка</div>
+      <div className="modal__close">
+        <button>
+          <img src={close} alt="close" />
+        </button>
+      </div>
+      <form className="modal__body">
+        <p>
+          <input type="radio" name="alphabet" value="alphabet" />
+          <label for="alphabet">По алфавиту</label>
+        </p>
+        <p>
+          <input type="radio" name="birthday" value="birthday" />
+          <label for="birthday">По дню рождения</label>
+        </p>
+      </form>
+    </div>
+  );
+};
 
-export default ModalFilter
+export default ModalFilter;
--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,67 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import ModalFilter from "../../Componets/Modal/ModalFilter";
 const Search = ({ loop, bar }) => {
+  const [modalActive, showActiv] = useState(true);
   return (
-    <div className="container">
-      <div className="search">
-        <div className="search__title">Поиск</div>
-        <form className="search__form">
-          <img src={loop} alt="loop" className="search__form-loop" />
-          <div className="search__form-input">
-            <input
-              name="search"
-              id="search"
-              type="text"
-              placeholder="Введи имя, тег, почту..."
-            />
-          </div>
-          <div className="search__form-btn">
-            <button>
-              <img src={bar} alt="bar" />
-            </button>
-          </div>
-        </form>
-        <div className="search__nav">
-          <NavLink
-            to={"all"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">Все</div>
-          </NavLink>
-          <NavLink
-            to={"designers"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">Designers</div>
-          </NavLink>
-          <NavLink
-            to={"analysts"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">Analysts</div>
-          </NavLink>
-          <NavLink
-            to={"managers"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">Managers</div>
-          </NavLink>
-          <NavLink
-            to={"ios"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">iOS</div>
-          </NavLink>
-          <NavLink
-            to={"android"}
-            className={({ isActive }) => (isActive ? "active-link" : " ")}
-          >
-            <div className="search__item">Android</div>
-          </NavLink>
-        </div>
-        <div className="divider"></div>
+    <>
+      <div className={modalActive ? "menu menu_active" : "menu"}>
+        <ModalFilter />
+        <div className="menu__overlay" onClick={() => showActiv(false)}></div>
       </div>
-    </div>
+      <div className="container">
+        <div className="search">
+          <div className="search__title">Поиск</div>
+          <form className="search__form">
+            <img src={loop} alt="loop" className="search__form-loop" />
+            <div className="search__form-input">
+              <input
+                name="search"
+                id="search"
+                type="text"
+                placeholder="Введи имя, тег, почту..."
+              />
+            </div>
+            <div className="search__form-btn">
+              <button>
+                <img src={bar} alt="bar" />
+              </button>
+            </div>
+          </form>
+          <div className="search__nav">
+            <NavLink
+              to={"all"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">Все</div>
+            </NavLink>
+            <NavLink
+              to={"designers"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">Designers</div>
+            </NavLink>
+            <NavLink
+              to={"analysts"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">Analysts</div>
+            </NavLink>
+            <NavLink
+              to={"managers"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">Managers</div>
+            </NavLink>
+            <NavLink
+              to={"ios"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">iOS</div>
+            </NavLink>
+            <NavLink
+              to={"android"}
+              className={({ isActive }) => (isActive ? "active-link" : " ")}
+            >
+              <div className="search__item">Android</div>
+            </NavLink>
+          </div>
+          <div className="divider"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
