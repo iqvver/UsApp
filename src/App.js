1820b949d8b64ac457db125b1f954c6094d641b2
iqvverVIVO <iqvver@gmail.com>
2022-10-07 12:47:24 +0200

    commit readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import SearchContainer from "./Pages/Search/SearchContainer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProfileUserContainer from "./Componets/ProfileUser/ProfileUserContainer";
 
 //компонента с роутами между главной стриницей и деталями пользователя
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/*" element={<SearchContainer />} />
+        <Route path="/" element={<Navigate replace to="/all" />} />
         <Route path="profile">
           <Route path=":userId" element={<ProfileUserContainer />} />
         </Route>
